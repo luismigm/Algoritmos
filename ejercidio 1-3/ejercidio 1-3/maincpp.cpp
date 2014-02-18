6aae3d589ffb8546cf187a6b551880e08d59b3c2
Luis Miguel García Martín <luismigm@hotmail.com>
2014-02-18 23:33:45 +0100

    método de ordenación por selección

--- ejercidio 1-3/ejercidio 1-3/maincpp.cpp
+++ ejercidio 1-3/ejercidio 1-3/maincpp.cpp
@@ -1,12 +1,22 @@
 #include <iostream> //entrada, salida
 #include <stdlib.h> //métodos random
-#include <time.h> //para usar la hora
+#include <time.h> //para usar las funciones de tiempo de la CPU
 
 using namespace std;
 
+/*Funcion para ordenar un array de enteros.
+ Parámetros:
+	 - arrayEntero: el array a ordenar
+	 - tamano: tamaño del array*/
+void ordenarArray(int * arrayEntero, int tamano);
+void mostrarArray(int * arrayEntero, int tamano);
+
+//Función principal donde creamos y ordenamos arrays
 void main()
 {
 	int tamano = 0;//en esta variable pondremos el tamaño del array
+	int timeInicio = 0;//variable donde se guarda el tiempo el que que se inicia un algoritmo
+	int timeFinal = 0;//variable donde se guarda el tiempo en el finaliza un algoritmo
 	/*el usuario introduce el tamaño del array*/
 	do
 	{
@@ -27,10 +37,45 @@ void main()
 
 	/*mostramos el array*/
 	cout << endl << "el vector original es el siguiente: " << endl;
+	mostrarArray(arrayEnteros, tamano);
+	cout << endl;
+
+	timeInicio=clock();
+	ordenarArray(arrayEnteros, tamano);
+	timeFinal=clock();
+
+	cout << endl << "el vector ordenado: " << endl;
+	mostrarArray(arrayEnteros, tamano);
+	cout << endl;
+
+	free (arrayEnteros);
+}
+
+void mostrarArray(int * arrayEntero, int tamano)
+{
 	for(int i = 0; i < tamano; i++)
 	{
-		cout << arrayEnteros[i] << " ";
+		cout << arrayEntero[i] << " ";
 	}
-	cout << endl;
-	free (arrayEnteros);
+}
+
+void ordenarArray(int * arrayEntero, int tamano)
+{
+	 int minimo = 0;//variable donde guardaremos la posicion de
+	 int enteroAuxiliar = 0;
+
+     for (int i = 0 ; i < tamano -1; i++) 
+     {
+		 minimo = i;
+		 for(int j = i+1; j < tamano; j++)
+		 {
+			if(arrayEntero[minimo] > arrayEntero[j])
+			 {
+				minimo = j;
+			 }
+		 }
+		 enteroAuxiliar = arrayEntero[minimo];
+		 arrayEntero[minimo] = arrayEntero[i];
+		 arrayEntero[i] = enteroAuxiliar ;
+    }
 }
